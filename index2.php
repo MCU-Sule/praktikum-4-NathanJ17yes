7216e5ba330119b70aceb07205579a8d50a0a4d8
NathanJ17yes <90676004+NathanJ17yes@users.noreply.github.com>
2021-10-14 13:02:39 +0700

    Add files via upload
    Revisi Index 2

---
+++ index2.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="author" content="NathanJoshua_2172026">
+    <title>index</title>
+</head>
+<body align = "center">
+    <table border ="3" align = "center" cellspacing = "0" >
+<form action = "jawabansoal2.php" method = "GET" target = "">
+    <tr>
+        <td colspan = "2" align = "center" bgcolor = "#DC143C">kalkulator</Td>
+        
+    </tr>
+    <tr>
+        <td bgcolor = "#FA8072">Angka pertama</Td>
+        <td><input type = "text" name = "a1" id = "a1"></Td>
+    </tr>
+    <tr>
+        <td bgcolor = "#FA8072">Angka kedua</Td>
+        <td><input type = "text" name = "a2" id = "a2"></Td>
+    </tr>
+    <tr>
+        <td bgcolor = "#FA8072">Operator</Td>
+        <td><input style = "text"  name = "op" id = "op">
+        </Td>
+    </tr>
+    <tr>
+       
+        <td colspan = "2" bgcolor = "#DC143C"><input type = "submit" value="submit"> <input type = "reset" value="reset"></Td>
+    </tr>
+</form>
+    </table>
+</body>
+</html>
